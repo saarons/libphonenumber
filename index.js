f614f179ce4d4eaac765bf92a2e600e06ba5ac3a
Sam Aarons <samaarons@gmail.com>
2013-01-15 22:58:50 -0800

    Zero One

---
+++ index.js
@@ -0,0 +1,20 @@
+var base_path, files, fs, path, vm;
+
+vm = require("vm");
+
+fs = require("fs");
+
+path = require("path");
+
+files = ["phonemetadata.pb.js", "phonenumber.pb.js", "metadata.js", "phonenumberutil.js", "asyoutypeformatter.js"];
+
+base_path = path.join(__dirname, "vendor/libphonenumber/i18n/phonenumbers/");
+
+module.exports = function(context) {
+  var file, _i, _len;
+  for (_i = 0, _len = files.length; _i < _len; _i++) {
+    file = files[_i];
+    vm.runInContext(fs.readFileSync(path.join(base_path, file), context, file));
+  }
+  return context.i18n;
+};
